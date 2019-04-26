1f75b130d209a01fda2364d332264d7e14165747
gumemura <gumemura123@gmail.com>
2019-04-26 18:47:48 -0300

    mrs anderson

--- fato.c
+++ fato.c
@@ -10,11 +10,11 @@
 #include <math.h>
 
 double senTaylor(double x){
-	int k = 1;
-	double termoAntigo, termoNovo, somatoria;
+	int k = 1, sinal;
+	/*double termoAntigo, termoNovo, somatoria;
 
-	somatoria = x;
 	termoAntigo = x;
+	somatoria = 0;
 
 	do{
 		termoNovo = termoAntigo * (-pow(x, 2)/((2*k)*((2*k)+1)));
@@ -22,10 +22,26 @@ double senTaylor(double x){
 
 		termoAntigo = termoNovo;
 		k++;
-	}while(termoAntigo <= pow(10, -11));
+	}while(termoAntigo <= pow(10, -8));*/
+	double numerador, denominador, somatoria;
+
+	numerador = x;
+	denominador = 1;
+	somatoria = 0;
+
+	while((numerador/denominador) >= pow(10, -8)){
+		somatoria += (numerador/denominador) * sinal;
+
+		sinal *= -1;
+		numerador *= x * x;
+		denominador *= ++k * (++k);
+	}
 
 	return somatoria;
 }
+
+
+
 
 /*float modulo(float transp){
 	if(transp < 0)
@@ -50,4 +66,11 @@ int main(){
 	scanf("%lf", &sen);
 
 	printf("Sen(%lf) = %.11lf", sen, senTaylor(sen));
+
+	printf("\n\n%f", sin(1));
+	printf("\n\n%f", sin(2));
+	printf("\n\n%f", sin(3));
+	printf("\n\n%f", sin(4));
+	printf("\n\n%f", sin(5));
+	printf("\n\n%f", sin(6));
 }
